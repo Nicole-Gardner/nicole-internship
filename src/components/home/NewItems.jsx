8fd0532819d45dee02f617834264fc1050dff432
Nicole Gardner <windatheart261392@gmail.com>
2026-07-18 13:01:30 -0500

    nicole_newitems

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -8,15 +8,28 @@ import nftImage from "../../images/nftImage.jpg";
 
 const settings = {
   dots: false,
-  infinite: true,
-  speed: 500,
-  slidesToShow: 4,
-  slidesToScroll: 1,
+  nav: true,
+  loop: true,
+  margin: 20,
+  items: 4,
+  responsive: {
+    0: {
+      items: 1,
+    },
+    768: {
+      items: 2,
+    },
+    992: {
+      items: 4,
+    },
+  },
 };
 
 const NewItems = () => {
   return (
     <section id="section-items" className="no-bottom">
+        <div className="nft__item"></div>
+        <div className="author_list_pp"></div>
       <div className="container">
         <div className="row">
           <div className="col-lg-12">
@@ -26,63 +39,46 @@ const NewItems = () => {
             </div>
           </div>
           <OwlCarousel className="owl-theme" {...settings}>
-                        {new Array(4).fill(0).map((_, index) => (
-                          <div key={index} style={{ padding: "0 5px" }}>
-                            <div className="nft_coll">
-                              <div className="nft_wrap">
-                  <Link
-                    to="/author"
-                    data-bs-toggle="tooltip"
-                    data-bs-placement="top"
-                    title="Creator: Monica Lucas"
-                  >
-                    <img className="lazy" src={AuthorImage} alt="" />
-                    <i className="fa fa-check"></i>
-                  </Link>
-                </div>
-                <div className="de_countdown">5h 30m 32s</div>
+                    
+{new Array(4).fill(0).map((_, index) => (
+  <div key={index}>
+    <div className="nft__item">
+      <div className="author_list_pp">
+        <Link
+          to="/author"
+          data-bs-toggle="tooltip"
+          data-bs-placement="top"
+          title="Creator: Monica Lucas"
+        >
+          <img className="lazy" src={AuthorImage} alt="" />
+          <i className="fa fa-check"></i>
+        </Link>
+      </div>
 
-                <div className="nft__item_wrap">
-                  <div className="nft__item_extra">
-                    <div className="nft__item_buttons">
-                      <button>Buy Now</button>
-                      <div className="nft__item_share">
-                        <h4>Share</h4>
-                        <a href="" target="_blank" rel="noreferrer">
-                          <i className="fa fa-facebook fa-lg"></i>
-                        </a>
-                        <a href="" target="_blank" rel="noreferrer">
-                          <i className="fa fa-twitter fa-lg"></i>
-                        </a>
-                        <a href="">
-                          <i className="fa fa-envelope fa-lg"></i>
-                        </a>
-                      </div>
-                    </div>
-                  </div>
+      <div className="de_countdown">5h 30m 32s</div>
 
-                  <Link to="/item-details">
-                    <img
-                      src={nftImage}
-                      className="lazy nft__item_preview"
-                      alt=""
-                    />
-                  </Link>
-                </div>
-                <div className="nft__item_info">
-                  <Link to="/item-details">
-                    <h4>Pinky Ocean</h4>
-                  </Link>
-                  <div className="nft__item_price">3.08 ETH</div>
-                  <div className="nft__item_like">
-                    <i className="fa fa-heart"></i>
-                    <span>69</span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          ))}
+      <div className="nft__item_wrap">
+        <Link to="/item-details">
+          <img src={nftImage} className="lazy nft__item_preview" alt="" />
+        </Link>
+      </div>
+
+      <div className="nft__item_info">
+        <Link to="/item-details">
+          <h4>Pinky Ocean</h4>
+        </Link>
+        <div className="nft__item_price">3.08 ETH</div>
+        <div className="nft__item_like">
+          <i className="fa fa-heart"></i>
+          <span>69</span>
+        </div>
+      </div>
+    </div>
+  </div>
+))}
           </OwlCarousel>
+          </div>
+          </div>
         </div>
       </div>
     </section>
